09e2cff00ee147798e72040386e401e1f0c584dc
Ming <soongming18@gmail.com>
2024-11-13 16:43:18 -0500

    added classes

--- src/src/Interface.java
+++ src/src/Interface.java
@@ -1,16 +1,16 @@
 import java.io.IOException;
 
 public class Interface {
-    GameMap gameMap;
+    MonsterGameMap gameMap;
     Party party;
 
     public Interface(int mapSize, int numPlayers) throws IOException {
         this.party = new Party(numPlayers);
-        this.gameMap = new GameMap(mapSize, this.party);
+        this.gameMap = new MonsterGameMap(mapSize, this.party);
     }
 
     public void startGame() throws IOException {
-        GamePlay gameplay = new GamePlay(this.party, this.gameMap);
+        MonsterGamePlay gameplay = new MonsterGamePlay(this.party, this.gameMap);
         while(true){
             System.out.println(gameMap);
             if(!gameplay.makeMove()){
